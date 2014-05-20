53bcf8c3a29b72a83dd22d60ac19dadb282617d9
Keenan Jaenicke <kjaenick@gmail.com>
2014-05-19 22:40:52 -0400

    save search results in localStorage

--- www/js/views/search-layout.js
+++ www/js/views/search-layout.js
@@ -31,6 +31,12 @@ define(['Backbone', 'Marionette', 'hbs!templates/search-layout', 'js/views/gameC
         var self = this;
         var s = $('#search_text').val();
         if(s.length > 0){
+          //save search terms
+          var recentSearches = window.localStorage.recentSearches ? JSON.parse(window.localStorage.recentSearches) : [];
+          recentSearches = [s].concat(recentSearches);
+          //limit the history to the last 25
+          window.localStorage.recentSearches = JSON.stringify(recentSearches.slice(0, 25));
+
           var gameCollection = new GameCollection([], { searchTerms: s });
           gameCollection.fetch({
             success: function(){
