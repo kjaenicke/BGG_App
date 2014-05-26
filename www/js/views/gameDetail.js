fb2385c58a388df856e2bbf6dfb161e1aaaee578
Keenan Jaenicke <kjaenick@gmail.com>
2014-05-26 09:11:47 -0400

    add photo gallery to game detail view, a little buggy still

--- www/js/views/gameDetail.js
+++ www/js/views/gameDetail.js
@@ -1,20 +1,47 @@
-define(['Backbone', 'Marionette', 'js/views/game', 'hbs!templates/game-details'],
-  function(Backbone, Marionette, GameView, template){
+define(['Backbone', 'Marionette', 'js/views/game', 'hbs!templates/game-details', 'js/models/photoCollection'],
+  function(Backbone, Marionette, GameView, template, PhotoCollection){
 
     var GameDetailView = Backbone.Marionette.ItemView.extend({
       ui: {
-        bookmark:  '.bookmark'
+        bookmark: '.bookmark'
       },
       events: {
-        'click @ui.bookmark'  : 'toggleBookmark'
+        'click @ui.bookmark'      : 'toggleBookmark'
       },
       initialize: function(options){
         this.model = this.options.model;
+        _.bindAll(this, 'fetchImages');
       },
       render: function(){
         var self = this;
         this.el = template(this.model.toJSON());
+        this.fetchImages();
         return this;
+      },
+      fetchImages: function(){
+        var self = this;
+
+        self.photoCollection = new PhotoCollection({ id: self.model.get('id') });
+        self.photoCollection.fetch({ success: function(){
+          var photoBrowserPhotos = [];
+
+          for(var i = 0; i < self.photoCollection.models.length; i++){
+            photoBrowserPhotos.push(self.photoCollection.models[i].get('url'));
+          }
+
+          self.photoBrowserStandalone = theApp.photoBrowser({
+              photos: photoBrowserPhotos
+          });
+
+          $(document).on('click', '.photoGallery', function(){
+            self.photoBrowserStandalone.open();
+          });
+
+          setTimeout(function(){
+              $('.photoGallery').show();
+          }, 100);
+          
+        }});
       },
       toggleBookmark: function(){
         var bookmarks;
