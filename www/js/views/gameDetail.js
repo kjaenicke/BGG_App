9db04cf7d1062460d8893daf8f665123004a375d
Shawn Hoffman (MBP) <Shawn.p.hoffman@gmail.com>
2014-05-26 10:04:58 -0400

    Game detail UI changes. Bookmark icon changed. Recent search icon changed.

--- www/js/views/gameDetail.js
+++ www/js/views/gameDetail.js
@@ -13,15 +13,16 @@ define(['Backbone', 'Marionette', 'js/views/game', 'hbs!templates/game-details']
       },
       render: function(){
         var self = this;
+        window.modelOutput = this.model;
         this.el = template(this.model.toJSON());
         return this;
       },
       toggleBookmark: function(){
         var bookmarks;
 
-        if($('.bookmark').hasClass('fa-star-o')){
-          $('.bookmark').removeClass('fa-star-o');
-          $('.bookmark').addClass('fa-star');
+        if($('.bookmark').hasClass('fa-bookmark-o')){
+          $('.bookmark').removeClass('fa-bookmark-o');
+          $('.bookmark').addClass('fa-bookmark');
 
           bookmarks = window.localStorage.bookmarks ? JSON.parse(window.localStorage.bookmarks) : [];
           bookmarks.push({ id: this.model.get('id'), title: this.model.get('title') });
@@ -30,8 +31,8 @@ define(['Backbone', 'Marionette', 'js/views/game', 'hbs!templates/game-details']
           return;
         }
         else{
-          $('.bookmark').removeClass('fa-star');
-          $('.bookmark').addClass('fa-star-o');
+          $('.bookmark').removeClass('fa-bookmark');
+          $('.bookmark').addClass('fa-bookmark-o');
 
           bookmarks = window.localStorage.bookmarks ? JSON.parse(window.localStorage.bookmarks) : [];
           var index = bookmarks.indexOf(this.model);
