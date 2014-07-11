ae45a3a3213dd6998c94acd62c14babf075feee0
Shawn Hoffman (MBP) <Shawn.p.hoffman@gmail.com>
2014-07-10 21:54:15 -0400

    Gruntfile stuff. run grunt to set stuff up for the iOS build.

--- www/Gruntfile.js
+++ www/Gruntfile.js
@@ -3,15 +3,57 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            src: ['bower_components/underscore/underscore.js'],
+            dest: 'bower_dist/underscore/',
+            filter: 'isFile', flatten: true
+          },
+          {
+            expand: true,
+            src: ['bower_components/handlebars/handlebars.amd.min.js'],
+            dest: 'bower_dist/handlebars/',
+            filter: 'isFile', flatten: true
+          },
+          {
+            expand: true,
+            cwd: 'bower_components/require-handlebars-plugin/',
+            src: ['hbs.js', 'hbs/*'],
+            dest: 'bower_dist/require-handlebars-plugin/'
+          },
+          {
+            expand: true,
+            src: ['bower_components/jquery/dist/*'],
+            dest: 'bower_dist/jquery',
+            filter: 'isFile', flatten: true
+          },
+          {
+            expand: true,
+            src: ['bower_components/marionette/lib/*'],
+            dest: 'bower_dist/marionette',
+            filter: 'isFile', flatten: true
+          },
+          {
+            expand: true,
+            src: ['bower_components/backbone/backbone.js'],
+            dest: 'bower_dist/backbone',
+            filter: 'isFile', flatten: true
+          }
+        ]
+      }
+    },
     jshint:{
       all: ['./js/**/*.js']
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['copy', 'jshint']);
 
 };
