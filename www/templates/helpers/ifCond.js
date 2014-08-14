4dbafea8c8cf29315fdd0021fbbcbdc88b68dfb2
Shawn Hoffman (MBP) <Shawn.p.hoffman@gmail.com>
2014-08-13 23:43:17 -0400

    Fixed issue with comment ratings.

---
+++ www/templates/helpers/ifCond.js
@@ -0,0 +1,30 @@
+define('templates/helpers/ifCond', ['hbs/handlebars'], function ( Handlebars ) {
+  function ifCond (v1, operator, v2, options) {
+    switch (operator) {
+      case '==':
+          return (v1 == v2) ? options.fn(this) : options.inverse(this);
+      case '===':
+          return (v1 === v2) ? options.fn(this) : options.inverse(this);
+      case '!=':
+          return (v1 != v2) ? options.fn(this) : options.inverse(this);
+      case '!==':
+          return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+      case '<':
+          return (v1 < v2) ? options.fn(this) : options.inverse(this);
+      case '<=':
+          return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+      case '>':
+          return (v1 > v2) ? options.fn(this) : options.inverse(this);
+      case '>=':
+          return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+      case '&&':
+          return (v1 && v2) ? options.fn(this) : options.inverse(this);
+      case '||':
+          return (v1 || v2) ? options.fn(this) : options.inverse(this);
+      default:
+          return options.inverse(this);
+    }
+  }
+  Handlebars.registerHelper( 'ifCond', ifCond );
+  return ifCond;
+});
