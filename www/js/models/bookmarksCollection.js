cb240ee6e3a95aeee4bb6d120fb4b45349a2e137
Shawn Hoffman (MBP) <Shawn.p.hoffman@gmail.com>
2014-10-10 16:00:40 -0400

    Sorting bookmarks alphabetically. Fixes #22

--- www/js/models/bookmarksCollection.js
+++ www/js/models/bookmarksCollection.js
@@ -3,6 +3,9 @@ define(['Backbone'],
 
     //COLLECTION OF USER'S BOOKMARKS
     var BookmarksCollection = Backbone.Collection.extend({
+      comparator: function(bookmark) {
+        return bookmark.get('title');
+      },
       fetch: function(options){
         var recentItems = window.localStorage.bookmarks ? JSON.parse(window.localStorage.bookmarks) : [];
         this.reset(recentItems);
